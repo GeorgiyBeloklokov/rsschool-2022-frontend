de9bb9ad42335716f8f5b702ebc36f122f6c2c6b
Georgiy_Beloklokov <georgiy.reus@gmail.com>
2022-10-05 00:12:30 +0300

    feat: change styles and files

--- online-zoo/pages/donate/index.js
+++ online-zoo/pages/donate/index.js
@@ -1,106 +1,104 @@
 window.addEventListener('load', () => {
-	const trackButton = document.querySelector('.donation__amount-track');
-	const pointsSpan = document.querySelectorAll('.donation__amount-point');
-	const buttonsActive = document.querySelectorAll('.donation__amount-button');
-	const dollars = document.querySelectorAll('.donation__amount-dollar');
-	const donationForm = document.querySelector('.donation__form');
-	const headerBurger = document.querySelector('.header-burger');
-	const burgerNav = document.querySelector('.burger-nav');
-	const burgerNavLinks = document.querySelectorAll('.burger-nav__link');
+  const trackButton = document.querySelector('.donation__amount-track');
+  const pointsSpan = document.querySelectorAll('.donation__amount-point');
+  const buttonsActive = document.querySelectorAll('.donation__amount-button');
+  const dollars = document.querySelectorAll('.donation__amount-dollar');
+  const donationForm = document.querySelector('.donation__form');
+  const headerBurger = document.querySelector('.header-burger');
+  const burgerNav = document.querySelector('.burger-nav');
+  const burgerNavLinks = document.querySelectorAll('.burger-nav__link');
 
-	burgerNavLinks.forEach((item) => {
-		item.addEventListener('click', () => {
-			let anim = burgerNav.animate(
-				[
-					{
-						opacity: 1,
-						transform: 'translateX(0%)',
-					},
-					{
-						opacity: 0,
-						transform: 'translateX(100%)',
-					},
-				],
-				{
-					duration: 200,
-				}
-			);
-			headerBurger.classList.remove('active--button');
+  burgerNavLinks.forEach((item) => {
+    item.addEventListener('click', () => {
+      let anim = burgerNav.animate(
+        [
+          {
+            opacity: 1,
+            transform: 'translateX(0%)',
+          },
+          {
+            opacity: 0,
+            transform: 'translateX(100%)',
+          },
+        ],
+        {
+          duration: 200,
+        }
+      );
+      headerBurger.classList.remove('active--button');
 
-			anim.addEventListener('finish', () => {
-				burgerNav.classList.remove('active--nav');
-				document.documentElement.classList.remove('_lock');
-			});
-		});
-	});
+      anim.addEventListener('finish', () => {
+        burgerNav.classList.remove('active--nav');
+        document.documentElement.classList.remove('_lock');
+      });
+    });
+  });
 
-	headerBurger.addEventListener('click', (e) => {
-		if (headerBurger.classList.contains('active--button')) {
-			let anim = burgerNav.animate(
-				[
-					{
-						opacity: 1,
-						transform: 'translateX(0%)',
-					},
-					{
-						opacity: 0,
-						transform: 'translateX(100%)',
-					},
-				],
-				{
-					duration: 200,
-				}
-			);
-			headerBurger.classList.remove('active--button');
+  headerBurger.addEventListener('click', (e) => {
+    if (headerBurger.classList.contains('active--button')) {
+      let anim = burgerNav.animate(
+        [
+          {
+            opacity: 1,
+            transform: 'translateX(0%)',
+          },
+          {
+            opacity: 0,
+            transform: 'translateX(100%)',
+          },
+        ],
+        {
+          duration: 200,
+        }
+      );
+      headerBurger.classList.remove('active--button');
 
-			anim.addEventListener('finish', () => {
-				burgerNav.classList.remove('active--nav');
-				document.documentElement.classList.remove('_lock');
-			});
-		} else {
-			burgerNav.classList.add('active--nav');
-			headerBurger.classList.toggle('active--button');
-			document.documentElement.classList.add('_lock');
-		}
-	});
+      anim.addEventListener('finish', () => {
+        burgerNav.classList.remove('active--nav');
+        document.documentElement.classList.remove('_lock');
+      });
+    } else {
+      burgerNav.classList.add('active--nav');
+      headerBurger.classList.toggle('active--button');
+      document.documentElement.classList.add('_lock');
+    }
+  });
 
-	donationForm.addEventListener('submit', (e) => {
-		e.preventDefault();
-	});
+  donationForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+  });
 
-	trackButton.addEventListener('click', (e) => {
-		let btnIdx = 0;
-		const target = e.target;
-		if (target.classList.contains('donation__amount-button')) {
-			pointsSpan.forEach((item) => item.classList.remove('point--active'));
-			target
-				.querySelector('.donation__amount-point')
-				.classList.add('point--active');
-			buttonsActive.forEach((item, i) => {
-				if (item === target) {
-					btnIdx = i;
-				}
-			});
-			dollars.forEach((item) => item.classList.remove('dollar--active'));
-			dollars[btnIdx].classList.add('dollar--active');
-		}
-	});
+  trackButton.addEventListener('click', (e) => {
+    let btnIdx = 0;
+    const target = e.target;
+    if (target.classList.contains('donation__amount-button')) {
+      pointsSpan.forEach((item) => item.classList.remove('point--active'));
+      target.querySelector('.donation__amount-point').classList.add('point--active');
+      buttonsActive.forEach((item, i) => {
+        if (item === target) {
+          btnIdx = i;
+        }
+      });
+      dollars.forEach((item) => item.classList.remove('dollar--active'));
+      dollars[btnIdx].classList.add('dollar--active');
+    }
+  });
 
-	if (window.innerWidth < 767) {
-		pointsSpan.forEach((item) => item.classList.remove('point--active'));
-		dollars.forEach((item) => item.classList.remove('dollar--active'));
-		dollars[5].classList.add('dollar--active');
-		pointsSpan[5].classList.add('point--active');
-	}
+  if (window.innerWidth < 767) {
+    pointsSpan.forEach((item) => item.classList.remove('point--active'));
+    dollars.forEach((item) => item.classList.remove('dollar--active'));
+    dollars[5].classList.add('dollar--active');
+    pointsSpan[5].classList.add('point--active');
+  }
 
-	const inputAmount = document.querySelector('.donation__form-amount');
+  const inputAmount = document.querySelector('.donation__form-amount');
 
-	const maxLength = +inputAmount.dataset.maxlength;
+  const maxLength = +inputAmount.dataset.maxlength;
 
-	inputAmount.addEventListener('input', (e) => {
-		inputAmount.value = inputAmount.value.replace(/[^0-9]/g, '');
-		if (inputAmount.value.length >= maxLength - 1) {
-			inputAmount.value = inputAmount.value.substring(0, maxLength);
-		}
-	});
+  inputAmount.addEventListener('input', (e) => {
+    inputAmount.value = inputAmount.value.replace(/[^0-9]/g, '');
+    if (inputAmount.value.length >= maxLength - 1) {
+      inputAmount.value = inputAmount.value.substring(0, maxLength);
+    }
+  });
 });
--- online-zoo/pages/main/index.js
+++ online-zoo/pages/main/index.js
@@ -1,61 +1,61 @@
 window.addEventListener('load', () => {
-	const headerBurger = document.querySelector('.header-burger');
-	const burgerNav = document.querySelector('.burger-nav');
-	const burgerNavLinks = document.querySelectorAll('.burger-nav__link');
+  const headerBurger = document.querySelector('.header-burger');
+  const burgerNav = document.querySelector('.burger-nav');
+  const burgerNavLinks = document.querySelectorAll('.burger-nav__link');
 
-	burgerNavLinks.forEach((item) => {
-		item.addEventListener('click', () => {
-			let anim = burgerNav.animate(
-				[
-					{
-						opacity: 1,
-						transform: 'translateX(0%)',
-					},
-					{
-						opacity: 0,
-						transform: 'translateX(100%)',
-					},
-				],
-				{
-					duration: 200,
-				}
-			);
-			headerBurger.classList.remove('active--button');
+  burgerNavLinks.forEach((item) => {
+    item.addEventListener('click', () => {
+      let anim = burgerNav.animate(
+        [
+          {
+            opacity: 1,
+            transform: 'translateX(0%)',
+          },
+          {
+            opacity: 0,
+            transform: 'translateX(100%)',
+          },
+        ],
+        {
+          duration: 200,
+        }
+      );
+      headerBurger.classList.remove('active--button');
 
-			anim.addEventListener('finish', () => {
-				burgerNav.classList.remove('active--nav');
-				document.documentElement.classList.remove('_lock');
-			});
-		});
-	});
+      anim.addEventListener('finish', () => {
+        burgerNav.classList.remove('active--nav');
+        document.documentElement.classList.remove('_lock');
+      });
+    });
+  });
 
-	headerBurger.addEventListener('click', (e) => {
-		if (headerBurger.classList.contains('active--button')) {
-			let anim = burgerNav.animate(
-				[
-					{
-						opacity: 1,
-						transform: 'translateX(0%)',
-					},
-					{
-						opacity: 0,
-						transform: 'translateX(100%)',
-					},
-				],
-				{
-					duration: 200,
-				}
-			);
-			headerBurger.classList.remove('active--button');
+  headerBurger.addEventListener('click', (e) => {
+    if (headerBurger.classList.contains('active--button')) {
+      let anim = burgerNav.animate(
+        [
+          {
+            opacity: 1,
+            transform: 'translateX(0%)',
+          },
+          {
+            opacity: 0,
+            transform: 'translateX(100%)',
+          },
+        ],
+        {
+          duration: 200,
+        }
+      );
+      headerBurger.classList.remove('active--button');
 
-			anim.addEventListener('finish', () => {
-				burgerNav.classList.remove('active--nav');
-				document.documentElement.classList.remove('_lock');
-			});
-		} else {
-			burgerNav.classList.add('active--nav');
-			headerBurger.classList.toggle('active--button');
-			document.documentElement.classList.add('_lock');
-		}
-	});
+      anim.addEventListener('finish', () => {
+        burgerNav.classList.remove('active--nav');
+        document.documentElement.classList.remove('_lock');
+      });
+    } else {
+      burgerNav.classList.add('active--nav');
+      headerBurger.classList.toggle('active--button');
+      document.documentElement.classList.add('_lock');
+    }
+  });
 });
