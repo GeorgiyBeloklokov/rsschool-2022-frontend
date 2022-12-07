c6fd5ed2aa1bdb4abf0fb122a564826e98a38943
Georgiy Beloklokov <gbeloklokov@ibagroup.eu>
2022-12-07 13:46:11 +0300

    fix: webpack conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,13 @@ module.exports = {
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
-  root: true,
-  env: {
-    "browser": true,
-    "amd": true,
-    "node": true
+    root: true,
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      env: {
+        browser: true,
+        amd: true,
+        node: true,
     },
-    "rules": {
-        "@typescript-eslint/no-explicit-any": "error"
-      }
-  };
+    },
+};
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,3 +1,70 @@
+const path = require('path');
+const HTMLWebpackPlugin = require ('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
 module.exports = {
-    mode: 'production',
-};
+    mode: "production",
+    entry: {
+        main: path.resolve(__dirname, './src/index.ts'),
+    },
+    performance: {
+      hints: false
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, 'dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.ts',
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+    },
+    module:{
+        rules: [
+            {
+                test: /\.(js|jsx|tsx|ts)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+           
+            {
+                test: /\.(css)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options:{
+                            publicPath: './',
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options:{
+                            url: false
+                        }
+                    },
+                    'postcss-loader'],
+            },
+        ],
+
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: 'news-js',
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'global.css',
+        }),
+        
+    ]
+}
