5a4209c43e775e7d56a148977ca69e0414eed36b
Georgiy Beloklokov <gbeloklokov@ibagroup.eu>
2022-12-11 19:33:49 +0300

    fix: url for deploy

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://nodenews.herokuapp.com/', {
+        super('https://nodenews.up.railway.app/', { 
             apiKey: '55cb6be843b74ce18532008211767630', // получите свой ключ https://newsapi.org/ //https://newsapi.org/v2/
         });
     }
